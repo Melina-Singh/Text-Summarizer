dedd8146fde7d32d83fdfbdf614b807272033a5d
Meli <melinacingh@gmail.com>
2025-01-16 16:34:06 +0545

    first commit

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+projectname = "textSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{projectname}/__init__.py",
+    f"src/{projectname}/components/__init__.py",
+    f"src/{projectname}/utils/__init__.py",
+    f"src/{projectname}/utils/common.py",
+    f"src/{projectname}/logging/__init__.py",
+    f"src/{projectname}/config/__init__.py",
+    f"src/{projectname}/config/configuration.py",
+    f"src/{projectname}/pipeline/__init__.py",
+    f"src/{projectname}/entity/__init__.py",
+    f"src/{projectname}/constants/__init__.py",
+    "config/config.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creatng directory:{filedir} for the file {filename}")
+
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already exists")
+
